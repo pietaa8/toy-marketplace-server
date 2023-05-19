0bdabbcca144f34ea578796ae03052d8378234df
priyab99 <priya.bd.9911@gmail.com>
2023-05-19 20:13:58 +0600

    delete done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app=express();
 const port=process.env.PORT|| 5000;
@@ -54,6 +54,13 @@ async function run() {
         const newToy=req.body;
         console.log(newToy);
         const result= await toyCollection.insertOne(newToy);
+        res.send(result);
+    });
+
+    app.delete('/toy/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result=await toyCollection.deleteOne(query);
         res.send(result);
     })
 
